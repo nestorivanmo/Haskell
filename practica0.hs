ea78e4f76bbd47e9290ed3558eb529590e19e43a
NESTOR IVAN MARTINEZ OSTOA <nestor.martinez.98@hotmail.com>
2019-08-31 11:03:46 -0500

    ...

---
+++ practica0.hs
@@ -0,0 +1,18 @@
+-- Ejercicio 1
+fibonacci :: Int -> Int
+fibonacci 0 = 0
+fibonacci 1 = 1
+fibonacci n = fibonacci (n - 1) + fibonacci (n - 2)
+
+sFibonacci :: Int -> [Int]
+sFibonacci x 
+	| x < 0 = []
+	| otherwise = [fibonacci i | i <- [0..x]]
+
+-- Ejercicio 2
+quitaElemento :: (Eq a) => [a] -> a -> [a]
+quitaElemento lista elemento = [e | e <- lista, e /= elemento]
+
+-- Ejercicio 3
+divisoresPropios :: Int -> [Int]
+divisoresPropios n = [i | i <- [1..(n-1)], n `mod` i == 0]
